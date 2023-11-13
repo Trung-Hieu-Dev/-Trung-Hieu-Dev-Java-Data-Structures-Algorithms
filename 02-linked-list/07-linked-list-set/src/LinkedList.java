34125bd972aa7ff6c8e590d1022df1c8aa0c391f
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-13 14:29:17 +0700

    LinkedList: Build My LinkedList Set Method - v2

--- 02-linked-list/07-linked-list-set/src/LinkedList.java
+++ 02-linked-list/07-linked-list-set/src/LinkedList.java
@@ -173,4 +173,12 @@ public class LinkedList {
     //                         //
     //                         //
     /////////////////////////////
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
 }
