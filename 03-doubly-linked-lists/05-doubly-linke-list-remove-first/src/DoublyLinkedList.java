7d9debf28d32a5c7c35adb66ca8f9cf9f43f8a2d
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-14 10:42:47 +0700

    DoublyLinkedList: Remove First - v2

--- 03-doubly-linked-lists/05-doubly-linke-list-remove-first/src/DoublyLinkedList.java
+++ 03-doubly-linked-lists/05-doubly-linke-list-remove-first/src/DoublyLinkedList.java
@@ -96,4 +96,18 @@ public class DoublyLinkedList {
     }
     
     // remove first
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
