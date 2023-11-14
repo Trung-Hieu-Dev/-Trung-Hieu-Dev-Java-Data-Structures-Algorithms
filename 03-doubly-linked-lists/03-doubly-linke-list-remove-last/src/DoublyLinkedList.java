7b9347cd640625b0652e526e5cda111edcb1e8c4
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-14 10:04:06 +0700

    DoublyLinkedList: Remove Last

---
+++ 03-doubly-linked-lists/03-doubly-linke-list-remove-last/src/DoublyLinkedList.java
@@ -0,0 +1,69 @@
+public class DoublyLinkedList {
+    
+    private Node head;
+    private Node tail;
+    private int length;
+    
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+    
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+    
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+    
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+    
+    // append
+    public void append(int value) {
+        // create new node
+        Node newNode = new Node(value);
+        
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+    
+    // remove last
+}
