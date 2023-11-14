22614dcff5b02b5e2b03489292c7bd88e8cff769
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-14 10:55:28 +0700

    DoublyLinkedList: Get - v2

--- 03-doubly-linked-lists/06-doubly-linke-list-get/src/DoublyLinkedList.java
+++ 03-doubly-linked-lists/06-doubly-linke-list-get/src/DoublyLinkedList.java
@@ -112,4 +112,19 @@ public class DoublyLinkedList {
     }
     
     // get
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        if (index < length/2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
 }
