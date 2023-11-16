eeda819afeb8fe37a19854ab65de07ee3ce17b0e
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-16 10:46:53 +0700

    Binary Search Tree: Contains

---
+++ 06-trees/03-binary-search-tree-contains/src/BinarySearchTree.java
@@ -0,0 +1,49 @@
+public class BinarySearchTree {
+    public Node root;
+    
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+        
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    
+    // insert: return boolean
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        
+        // Case 1: Empty
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        
+        // Case 2: Not empty
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            // Insert left if value < root
+            if (newNode.value < temp.value) {
+                // Case left has no leaf
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                // move temp to next
+                temp = temp.left;
+            } else { // Insert left if value > root
+                // Case left has no leaf
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                // move temp to next
+                temp = temp.right;
+            }
+        }
+        
+    }
+}
