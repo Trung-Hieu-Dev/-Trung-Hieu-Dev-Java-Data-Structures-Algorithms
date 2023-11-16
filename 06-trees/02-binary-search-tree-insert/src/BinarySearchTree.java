c638a1e1cf996ac415505f1181e2caf42ef9e29a
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-16 10:08:53 +0700

    Binary Search Tree: Insert

---
+++ 06-trees/02-binary-search-tree-insert/src/BinarySearchTree.java
@@ -0,0 +1,13 @@
+public class BinarySearchTree {
+    public Node root;
+    
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+        
+        Node(int value) {
+            this.value = value;
+        }
+    }
+}
