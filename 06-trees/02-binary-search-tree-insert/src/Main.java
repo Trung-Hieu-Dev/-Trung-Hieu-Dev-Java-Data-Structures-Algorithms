81b0aecc373a8d86b2b9749e7510ef2737f25669
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-16 10:45:09 +0700

    Binary Search Tree: Insert - v2

--- 06-trees/02-binary-search-tree-insert/src/Main.java
+++ 06-trees/02-binary-search-tree-insert/src/Main.java
@@ -2,15 +2,33 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
         
+        myBST.insert(2);
+        myBST.insert(1);
+        myBST.insert(3);
+
+        /*
+            THE LINES ABOVE CREATE THIS TREE:
+                         2
+                        / \
+                       1   3
+        */
         
-        // ROOT MUST BE PUBLIC FOR THIS LINE TO WORK
-        System.out.println("Root: " + myBST.root);
+        
+        
+        // ROOT MUST BE PUBLIC FOR THESE LINES TO WORK
+        System.out.println("Root: " + myBST.root.value);
+        System.out.println("\nRoot->Left: " + myBST.root.left.value);
+        System.out.println("\nRoot->Right: " + myBST.root.right.value);
 
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Root: null
+            Root: 2
+
+            Root->Left: 1
+
+            Root->Right: 3
 
         */
     }
