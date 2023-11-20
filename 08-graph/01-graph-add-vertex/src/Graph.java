1e7ac29340d308ea4c697e05f1e61a431c6c1660
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-20 21:48:24 +0700

    Graph - Add Vertex - v2

--- 08-graph/01-graph-add-vertex/src/Graph.java
+++ 08-graph/01-graph-add-vertex/src/Graph.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Graph {
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+    
+    // print graph
+    public void printGraph() {
+        System.out.println(this.adjList);
+    }
+    
+    // add vertex
+    public boolean addVertex(String vertex) {
+        if (this.adjList.get(vertex) == null) {
+            this.adjList.put(vertex, new ArrayList<>());
+            return true;
+        }
+        return false;
+    }
 }
--- 08-graph/01-graph-add-vertex/src/Main.java
+++ 08-graph/01-graph-add-vertex/src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        
+        System.out.println("\nGraph:");
+        myGraph.printGraph();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Graph:
+            {A=[], B=[]}
+
+        */
+    }
 }
