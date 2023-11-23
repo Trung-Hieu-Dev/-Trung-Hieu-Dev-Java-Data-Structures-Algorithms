c66dc4ec92b4e46135a33bfc28c77d664ae608ee
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-11-23 22:34:51 +0700

    Tree Traversal: Depth First Search - PreOder

---
+++ 11-tree-traversal/02-deapth-first-search-pre-order/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+        
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+        
+        System.out.println(myBST.BFS());
+        
+        /*
+        * EXPECTED OUTPUT
+        * ---------------
+        * [47, 21, 76, 18, 27, 52, 82]
+        *
+        * */
+    }
+}
